efea89d63e0a45f9effcc0e008491d9126a39d8e
Rodrigo Guevara <roalgenio14@gmail.com>
2025-07-15 15:45:06 -0500

    removed dependencies

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import joblib
 import os
-import pingouin as pg
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
